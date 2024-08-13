96ff60ec0d1ad47bb8bd1da1d1460827279f315d
Mukilan Krishnakumar <mukilan.git@gmail.com>
2024-08-13 07:46:35 +0530

    Sizeof Types

---
+++ 1_The_Basics/type_check.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Size of Char " << sizeof(char) << "\n";
+  cout << "Size of Int " << sizeof(int) << "\n";
+  cout << "Size of Bool " << sizeof(bool) << "\n";
+  cout << "Size of Double " << sizeof(double) << "\n";
+}
