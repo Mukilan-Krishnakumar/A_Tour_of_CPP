8d3a6b0e3e716d3e02ac080de1abac637e455094
Mukilan Krishnakumar <mukilan.git@gmail.com>
2024-08-13 07:54:05 +0530

    Init of Complex Types

--- 1_The_Basics/type_check.cpp
+++ 1_The_Basics/type_check.cpp
@@ -1,4 +1,6 @@
+#include <complex>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,4 +9,15 @@ int main() {
   cout << "Size of Int " << sizeof(int) << "\n";
   cout << "Size of Bool " << sizeof(bool) << "\n";
   cout << "Size of Double " << sizeof(double) << "\n";
+
+  complex<double> i = 1;
+  cout << "Complex value " << i << "\n";
+
+  vector<int> v{1, 2, 3, 4, 5, 6};
+  cout << "Vector Size " << sizeof(v) << "\n";
+
+  cout << "Vector values \n";
+  for (int i = 0; i < v.size(); i++) {
+    cout << "Value: " << i << "\n";
+  }
 }
